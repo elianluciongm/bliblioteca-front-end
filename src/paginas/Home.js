907cab48b929789bc3fb08db91d29bc768bb4378
elianluciongm <108909831+elianluciongm@users.noreply.github.com>
2025-05-13 22:00:02 -0300

    Add files via upload

---
+++ src/paginas/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+        <h1>Cards dos livros</h1>
+        </>
+    );
+}
---
+++ src/paginas/ListaCategoria.js
@@ -0,0 +1,11 @@
+import TituloLista from "../componentes/TituloLista";
+
+export default function ListaCategoria() {
+    return (
+        <>
+        <TituloLista titulo="Categorias"
+        descrição="Gerencie aqui as categorias dos livros da biblioteca"
+        rota= "/catergoria" />
+        </>
+    );
+}
